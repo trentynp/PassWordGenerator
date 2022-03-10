1533cf59e0d5897a0c1b2755632b5d90adcb3566
Trentyn <trentynpowell@gmail.com>
2022-03-10 10:36:49 -0800

    characters

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
+
+//Characters for passwords
+character = ["!", "#", "$", "%", "&","+","-", ".", "/","=","?","@","_","|","~"];
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
