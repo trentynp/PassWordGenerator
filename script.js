150903a4332b5ad5dea714866812b048a2afbb55
Trentyn <trentynpowell@gmail.com>
2022-03-10 10:42:55 -0800

    CSS

--- script.js
+++ script.js
@@ -102,14 +102,12 @@ function generatePassword() {
         var pickChoices = choices[Math.floor(Math.random() * choices.length)];
         password.push(pickChoices);
     }
-    // This joins the password array and converts it to a string
-    // Worked with a tutor to incorporate this option
+   
     var ps = password.join("");
     UserInput(ps);
     return ps;
 }
-// This puts the password value into the textbox
-// Changed function input to use textcontent
+
 function UserInput(ps) {
     document.getElementById("password").textContent = ps;
 
