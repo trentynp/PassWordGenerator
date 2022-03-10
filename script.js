1b2a9bf530a2fb074930a273abbf8a883f732c22
Trentyn <trentynpowell@gmail.com>
2022-03-10 10:38:08 -0800

    uppercase

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ character = ["!", "#", "$", "%", "&","+","-", ".", "/","=","?","@","_","|","~"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var choices;
+ 
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
+
+alpha2 = alpha.map(toUpper);
+
+var get = document.querySelector("#generate");
+
+get.addEventListener("click", function () {
+    ps = generatePassword();
+    document.getElementById("password").placeholder = ps;
+});
